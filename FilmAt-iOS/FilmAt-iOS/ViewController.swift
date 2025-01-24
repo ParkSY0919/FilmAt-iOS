4518bb29b93684dbad910898ca62adc0831c99d1
ParkSY0919 <qhr498@gmail.com>
2025-01-24 23:00:39 +0900

    [Setting/#1] init Project

---
+++ FilmAt-iOS/FilmAt-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FilmAt-iOS
+//
+//  Created by 박신영 on 1/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
