9c356cd90738f74253a2f84e8bb4194502da5eba
ParkSY0919 <qhr498@gmail.com>
2025-01-28 18:16:28 +0900

    [Add/#17] add SearchView Related files

---
+++ FilmAt-iOS/FilmAt-iOS/Presentation/Search/View/SearchView.swift
@@ -0,0 +1,23 @@
+//
+//  SearchView.swift
+//  FilmAt-iOS
+//
+//  Created by 박신영 on 1/28/25.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class SearchView: BaseView {
+    
+    override func setHierarchy() {}
+    
+    override func setLayout() {}
+    
+    override func setStyle() {
+        
+    }
+    
+}
---
+++ FilmAt-iOS/FilmAt-iOS/Presentation/Search/View/SearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchViewController.swift
+//  FilmAt-iOS
+//
+//  Created by 박신영 on 1/28/25.
+//
+
+import UIKit
+
+final class SearchViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
