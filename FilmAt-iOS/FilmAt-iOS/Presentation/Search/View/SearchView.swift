91d6ecde4d5f4d4ff0fb1680c3fc4c6d3d89816d
ParkSY0919 <qhr498@gmail.com>
2025-01-28 18:55:26 +0900

    [Style/#17] 상단 검색바 UI 구현

--- FilmAt-iOS/FilmAt-iOS/Presentation/Search/View/SearchView.swift
+++ FilmAt-iOS/FilmAt-iOS/Presentation/Search/View/SearchView.swift
@@ -12,12 +12,35 @@ import Then
 
 final class SearchView: BaseView {
     
-    override func setHierarchy() {}
+    let searchTextField = UITextField()
     
-    override func setLayout() {}
+    override func setHierarchy() {
+        self.addSubviews(searchTextField)
+    }
+    
+    override func setLayout() {
+        searchTextField.snp.makeConstraints {
+            $0.top.equalTo(safeAreaLayoutGuide)
+            $0.horizontalEdges.equalToSuperview().inset(10)
+            $0.height.equalTo(40)
+        }
+    }
     
     override func setStyle() {
         
+        searchTextField.do {
+            $0.backgroundColor = .searchBarBackground
+            $0.font = .filmAtFont(.body_regular_16)
+            $0.textColor = UIColor(resource: .title)
+            $0.setPlaceholder(placeholder: " 영화를 검색해보세요.",
+                              fontColor: UIColor(resource: .gray1),
+                              font: .filmAtFont(.body_regular_16))
+            $0.setLeftPadding(amount: 25,
+                              image: UIImage(systemName: "magnifyingglass"),
+                              inset: 8)
+            $0.layer.cornerRadius = 10
+        }
     }
     
 }
+
