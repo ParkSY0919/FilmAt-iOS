1583cc61582bf2cc8ddccd4edbcd0215b0901ad4
ParkSY0919 <qhr498@gmail.com>
2025-01-29 13:18:39 +0900

    [Feat/#21] 탈퇴하기 기능 구현

--- FilmAt-iOS/FilmAt-iOS/Presentation/Setting/View/SettingViewController.swift
+++ FilmAt-iOS/FilmAt-iOS/Presentation/Setting/View/SettingViewController.swift
@@ -58,6 +58,24 @@ private extension SettingViewController {
 
 extension SettingViewController: UITableViewDelegate {
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.row {
+        case 3:
+            let doneAction = UIAlertAction(title: "확인", style: .default) { _ in
+                print("탈퇴 누름!")
+                //유저디폴츠 값 초기화
+                UserDefaults.standard.removePersistentDomain(forName: Bundle.main.bundleIdentifier!)
+                self.viewTransition(viewController: OnBoardingViewController(), transitionStyle: .pushWithRootVC)
+            }
+            let alert = UIAlertManager.showAlertWithAction(title: "탈퇴하기",
+                                                           message: "탈퇴를 하면 데이터가 모두 초기화됩니다. 탈퇴 하시겠습니까?",
+                                                           cancelFunc: true, doneAction: doneAction)
+            present(alert, animated: true)
+        default:
+            print("해당 cell(\(indexPath.row)은 동작하지 않습니다")
+        }
+    }
+    
 }
 
 extension SettingViewController: UITableViewDataSource {
