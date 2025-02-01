52075f413f281eb9faf167fd4928af8ecc9ed902
ParkSY0919 <qhr498@gmail.com>
2025-02-01 19:27:34 +0900

    [Chore/#29] 주석 및 질문

---
+++ FilmAt-iOS/FilmAt-iOS/Global/Literals/StringLiterals.swift
@@ -0,0 +1,40 @@
+//
+//  StringLiterals.swift
+//  FilmAt-iOS
+//
+//  Created by 박신영 on 2/1/25.
+//
+
+import Foundation
+
+/**
+ 질문:
+ - 보통 아래와 같이 stringLiterals로 앱 내에서 쓰이는 string 값을 enum으로 저장하여 해당 값이 쓰이는 곳에 할당해 쓰고 있는데,
+   이 방식이 좋은 방향일까요..? 이번에는 여차저차 하다가 적용하지 못 하였습다.. [ (_ _) ]
+ */
+
+//enum StringLiterals {
+//    
+//    enum abc {
+//        static let nicknamePlaceholderLabel = "한글, 영문 1자 이상, 7자 이하"
+//    }
+//    
+//    enum def {
+//        static let ownerNoticeWordLabel = "참여자가 다 모이면 확인 버튼을 눌러주세요!"
+//        static let removeMemberSubLabel = "지금 삭제해도 다시 초대할 수 있어요!"
+//        static let deleteBottomSheetLeftBtnLabel = "아니요"
+//        static let deleteBottomSheetRightBtnLabel = "삭제"
+//        static let confirmBottomSheetLeftBtnLabel = "취소"
+//        static let confirmBottomSheetRightBtnLabel = "확정"
+//    }
+//    
+//    enum ghi {
+//        static let titleLabel = "방 입장하기"
+//        static let subTitleLabel1 = "두근두근"
+//        static let subTitleLabel2 = "나만의 마니또를 만나러 - !"
+//        static let bottomSheetTitleLabel = "방 참여코드"
+//        static let bottomSheetSubTitleLabel = "방장에게 받은 참여 코드를 입력해주세요"
+//        static let bottomSheetPlaceholder = "참여 코드"
+//    }
+//    
+//}
